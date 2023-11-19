6f0ad16fb5ef88bfde472fcc0bced3a5e37ce44c
Carla de Oliveira <carlaolive999@gmail.com>
2023-11-19 16:14:02 -0300

    conexão e printar os dados da tabela

--- Php/arquivo.php
+++ Php/arquivo.php
@@ -1,17 +1,29 @@
 <?php 
-include_once('conexaoBD.php');
-$conteudoJS = file_get_contents('php://input');
+
+require_once('config.php');
 
 
-function cadastrar($conteudoJS ){
-    $jsonJSArray = json_decode($conteudoJS, true);
+$statement = $pdo->query('select * from produtos.db_produtos;');
 
-    $produto = $jsonJSArray['produto'];
-    $quantidade = $jsonJSArray['quantidade'];
-    $unidade = $jsonJSArray['unidade'];
-    $preco_compra  = $jsonJSArray['precoInicial'];
-    $preco_venda = $jsonJSArray['precoFinal'] ;
-    
-    // $result = pg_query($conexao,"INSERT INTO ");
+
+
+while (($row = $statement->fetch(PDO::FETCH_ASSOC))) {
+    print_r($row);
+    echo '<br>';
 }
+
+
+// $conteudoJS = file_get_contents('php://input');
+
+
+// function cadastrar($conteudoJS ){
+//     $jsonJSArray = json_decode($conteudoJS, true);
+
+//     $produto = $jsonJSArray['produto'];
+//     $quantidade = $jsonJSArray['quantidade'];
+//     $unidade = $jsonJSArray['unidade'];
+//     $preco_compra  = $jsonJSArray['precoInicial'];
+//     $preco_venda = $jsonJSArray['precoFinal'] ;
+
+// }
 ?>
---
+++ Php/config.php
@@ -0,0 +1,23 @@
+<?php
+$host= 'localhost';
+$db = 'db_produtos';
+$user = 'postgres';
+$password = '1234';
+
+try {
+	$dsn = "pgsql:host=$host;port=5432;dbname=$db;";
+	// make a database connection
+	$pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+	if ($pdo) {
+		echo "Connected to the $db database successfully!";
+	}
+} catch (PDOException $e) {
+	die($e->getMessage());
+} 
+
+
+
+
+
+?>
